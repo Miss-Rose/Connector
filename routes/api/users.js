edc4f2e23f6cba3b62c49c106ff3fd1f5ca1496a
Tetiana.Stepanova <Tetiana.Stepanova@infopulse.com>
2021-02-10 19:11:54 +0200

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,25 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcrypt');
 const {check, validationResult} = require('express-validator');
+
+const User = require('../../modules/User');
+
 
 // @route api/users - public
 // @desc  register user
-router.post('/',[
-    check('name', 'Name is required')
-        .not()
-        .isEmpty(),
-    check('email', 'Put valid email')
-        .isEmail(),
-    check('password', 'Put valid email')
-        .isLength({min: 6})
+router.post('/', [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty(),
+        check('email', 'Put valid email')
+            .isEmail(),
+        check('password', 'Put valid email')
+            .isLength({min: 6})
     ],
-    (req, res) => {
-    console.log(req.body);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()});
-    }
-        res.send('User route')
+    async (req, res) => {
+        console.log(req.body);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const {name, email, password} = req.body;
+        try {
+            //See if user exists
+                let user = await User.findOne({email});
+                if(user){
+                    res.status(400).json({errors: [{msg: 'user already exist'}]});
+                }
+            //Get user gravatar
+                const avatar = gravatar.url(email, {
+                    s: '200',
+                    r: 'pg',
+                    d: 'mm'
+                });
+                user = new User ({
+                    name,
+                    email,
+                    avatar,
+                    password
+                });
+            //Encrypt password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+                await user.save();
+            //Return jsonwebtokcken
+
+            res.send('User registered')
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
     }
 );
 
